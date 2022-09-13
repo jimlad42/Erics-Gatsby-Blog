f6f587ff8e809c3a8a452250f8ed26826324917a
Eric <eric_penrod1@mymail.eku.edu>
2022-09-13 10:40:22 -0400

    Starting commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Erics Test Blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
