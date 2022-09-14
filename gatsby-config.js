3b0d1f8932758f4362fe0e5626b17d42dc87a4fb
Eric <eric_penrod1@mymail.eku.edu>
2022-09-13 21:11:36 -0400

    Finished part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,17 @@ module.exports = {
     title: `Erics Test Blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
+  plugins: [
+    "gatsby-plugin-mdx", 
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+  },
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  ]
 };
